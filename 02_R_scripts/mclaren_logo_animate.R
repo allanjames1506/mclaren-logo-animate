f043085af323fda1fccb224bae1dcf74ec783fb0
Allan James <allanjames1506@gmail.com>
2024-02-09 22:26:53 +0000

    commit form RStudio

--- 02_R_scripts/mclaren_logo_animate.R
+++ 02_R_scripts/mclaren_logo_animate.R
@@ -2,6 +2,8 @@
 
 # https://stackoverflow.com/questions/71704465/spatial-operations-how-to-buffer-a-polygon-by-area-not-distance-from-edge
 
+# 1 Libraries----
+
 library(tidyverse)
 library(sf)
 library(gganimate)
@@ -22,6 +24,7 @@ showtext_auto()
 showtext_opts(dpi = 300)
 
 # ANIMATE: McLaren speedmark buffered polygons----
+
 # grid_plot <- expand.grid(lon = seq(-180, 180, 10), 
 #                          lat = seq(-90, 90, 10))
 # 
@@ -394,6 +397,129 @@ test_fct_aj2 <- function(a, i){
   
 }
 
+p_gnat <- map_dfr(1:10, ~data_frame(y = seq(1, .x, length.out = 10), t = 1:10))
+
+make_seq <- function(x) {
+  seq <- seq(x, 2*x, length.out = 10)
+}
+
+out_lon <- vector("list", length(grid_mclaren_simple$lon))
+for (i in seq_along(grid_mclaren_simple$lon)) {
+  out_lon[[i]] <- make_seq(grid_mclaren_simple$lon[[i]])
+}
+
+lons_unlisted <- purrr::map_df(out_lon, tibble::as_tibble) %>% 
+  rename(lon = value) %>% 
+  mutate(t = rep(1:10, length.out = nrow(.)))
+
+out_lat <- vector("list", length(grid_mclaren_simple$lat))
+for (i in seq_along(grid_mclaren_simple$lat)) {
+  out_lat[[i]] <- make_seq(grid_mclaren_simple$lat[[i]])
+}
+
+lats_unlisted <- purrr::map_df(out_lat, tibble::as_tibble) %>% 
+  rename(lat = value) %>% 
+  mutate(t = rep(1:10, length.out = nrow(.)))
+
+lon_lat <- lons_unlisted %>%
+  select(-t) %>% 
+  bind_cols(lats_unlisted)
+
+scatter_plot_animate_lon_lat <- lon_lat %>% 
+  ggplot(aes(lon, lat)) + 
+  geom_point(color = '#E27231', size = 2) +
+  #with_outer_glow(geom_point(color = '#E27231', size =2), colour='gold', sigma = 5, expand = 5) +
+  transition_time(t) + 
+  shadow_wake(0.5) 
+#+annotate("text", y = 300, x = 300, label = "@allanjames1506", lineheight = 0.75, family = 'Zen',  size = 5, color = "gray40", vjust = 0.5)
+
+animate(scatter_plot_animate_lon_lat, fps = 30)
+
+anim_save("./04_animate_gifs/fourth_saved_animation_logo_animate.gif", height = 372, width = 538, units = "px")
+
+make_seq2 <- function(value) {
+  seq <- map_dfr(1:3, ~crossing(x = 1:84, nesting(
+    y = seq(value, .x, length.out = 3)^0.5, 
+    t = 1:3
+  )))
+}
+
+out_lon2 <- vector("list", length(grid_mclaren_simple$lon))
+for (i in seq_along(grid_mclaren_simple$lon)) {
+  out_lon2[[i]] <- make_seq2(grid_mclaren_simple$lon[[i]])
+}
+
+lons2_unlisted <- purrr::map_df(out_lon2, tibble::as_tibble)  
+
+scatter_plot_animate_random <- lons2_unlisted %>% 
+  ggplot(aes(x, y)) + 
+  geom_point(color = '#E27231', size = 2) +
+  #with_outer_glow(geom_point(color = '#E27231', size =2), colour='gold', sigma = 5, expand = 5) +
+  transition_time(t) + 
+  shadow_wake(0.5) 
+
+animate(scatter_plot_animate_random, fps = 30)
+
+
+make_seq3 <- function(value) {
+  seq <- crossing(x = value, nesting(t = 10:1, y = 0.75^(seq(t))))
+}
+
+out_lon3 <- vector("list", length(grid_mclaren_simple$lon))
+for (i in seq_along(grid_mclaren_simple$lon)) {
+  out_lon3[[i]] <- make_seq3(grid_mclaren_simple$lon[[i]])
+}
+
+lons3_unlisted <- purrr::map_df(out_lon3, tibble::as_tibble) %>% 
+  mutate(xy = x*y) %>% 
+  rename(lon = xy) %>% 
+  select(t, lon)
+
+out_lat3 <- vector("list", length(grid_mclaren_simple$lat))
+for (i in seq_along(grid_mclaren_simple$lat)) {
+  out_lat3[[i]] <- make_seq3(grid_mclaren_simple$lat[[i]])
+}
+
+lats3_unlisted <- purrr::map_df(out_lat3, tibble::as_tibble) %>% 
+  mutate(xy = x*y) %>% 
+  rename(lat = xy) %>% 
+  select(lat)
+
+lons_lats3 <- lons3_unlisted %>% 
+  bind_cols(lats3_unlisted)
+
+scatter_plot_animate_hole <- lons_lats3 %>% 
+  ggplot(aes(lon, lat)) + 
+  #geom_point(color = '#E27231', size = 2) +
+  with_outer_glow(geom_point(color = '#E27231', size =2), colour='#47c7fc', sigma = 2, expand = 2) +
+  transition_time(t) + 
+  shadow_wake(0.9) 
+
+animate(scatter_plot_animate_hole, nframes = 150, fps=30)
+
+
+make_seq(10)
+
+lons <- c(26.78571, 31.42857, 36.07143)
+
+longitudes <- vector(mode="numeric", length=length(lons))
+for (i in 1:length(lons)){
+  lon_edit <- lons[i] * 2
+  longitudes[i] <- lon_edit
+}
+
+test_map_dbl <- map_dbl(.x = grid_mclaren_simple$lon, ~make_seq)
+seq_along(grid_mclaren_simple$lon)
+
+means <- c(0, 1, 2)
+
+out <- vector("list", length(means))
+for (i in seq_along(means)) {
+  n <- sample(100, 1)
+  out[[i]] <- rnorm(n, means[[i]])
+}
+str(out)
+
 test_fct_aj3 <- function(a, i){
   
   sample(a:i, 10, replace = TRUE)
@@ -415,7 +541,7 @@ scatter_plot_animate4 <- mc_test_aj3 %>%
   transition_time(t) + 
   shadow_wake(0.5)
 
-animate(scatter_plot_animate4, fps = 30)
+animate(scatter_plot_animate4, nframes = 30)
 
 
 test_30 <- grid_mclaren_slice %>% 
@@ -452,5 +578,213 @@ scatter_plot_animate5 <- mc_test_aj6 %>%
 
 animate(scatter_plot_animate5, fps = 30)
 
+# ANIMATE: McLaren heart third phase----
 
+grid_mclaren_heart1 <- read.csv('./00_raw_data/mclaren_logo_heart1_dots_horizontal_flip.csv') %>% 
+  select(2:3) %>% 
+  rename(lon = x, lat = y)
+
+p_mclaren_heart1 <- grid_mclaren_heart1 %>% 
+  ggplot(aes(lon, lat)) + 
+  geom_point(color = 'white') 
+
+p_mclaren_heart1
+
+p_mclaren_heart1_polygon <- grid_mclaren_heart1 %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
+  summarise(geometry = st_combine(geometry)) %>%
+  st_cast("POLYGON")
+
+st_centroid(p_mclaren_heart1_polygon)
+
+centroid_longitude_heart1 <- 143.7585
+centroid_latitude_heart1 <- 119.9406
+
+# grid_mclaren_heart1_slice <- grid_mclaren_heart1 %>% 
+#   slice(1:63)
+
+grid_mclaren_heart1 |>
+  group_split(lon, lat) %>% 
+  map_dfr(~tibble(t= 1:10,
+                  lon = .x$lon, 
+                  lat = .x$lat,
+                  lon_vals = test_fct_aj(centroid_longitude_heart1, .x$lon),
+                  lat_vals = test_fct_aj(centroid_latitude_heart1, .x$lat))) -> mc_test_aj_heart1
+
+# find mid-colours btween deeppink and papaya
+# https://meyerweb.com/eric/tools/color-blend/#FF1493:E27231:2:hex
+
+scatter_plot_animate_heart1 <- mc_test_aj_heart1 %>% 
+  ggplot(aes(lon_vals, lat_vals)) + 
+  geom_point(color = '#EC5352', size =2) +
+  #with_outer_glow(geom_point(color = '#E27231', size =2), colour='gold', sigma = 5, expand = 5) +
+  transition_time(t) + 
+  shadow_wake(0.5) +
+  annotate("text", y = 70, x = 170, label = "@allanjames1506", lineheight = 0.75, family = 'Zen',  size = 5, color = "gray40", vjust = 0.5)
+
+animate(scatter_plot_animate_heart1, fps = 30)
+
+# ANIMATE: McLaren heart second phase----
+
+grid_mclaren_heart2 <- read.csv('./00_raw_data/mclaren_logo_heart2_dots_horizontal_flip.csv') %>% 
+  select(2:3) %>% 
+  rename(lon = x, lat = y)
+
+p_mclaren_heart2 <- grid_mclaren_heart2 %>% 
+  ggplot(aes(lon, lat)) + 
+  geom_point(color = 'white') 
+
+p_mclaren_heart2
+
+p_mclaren_heart2_polygon <- grid_mclaren_heart2 %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
+  summarise(geometry = st_combine(geometry)) %>%
+  st_cast("POLYGON")
+
+st_centroid(p_mclaren_heart2_polygon)
+
+centroid_longitude_heart2 <- 145.2814
+centroid_latitude_heart2 <- 121.1838
+
+# grid_mclaren_heart1_slice <- grid_mclaren_heart1 %>% 
+#   slice(1:63)
+
+grid_mclaren_heart2 |>
+  group_split(lon, lat) %>% 
+  map_dfr(~tibble(t= 1:10,
+                  lon = .x$lon, 
+                  lat = .x$lat,
+                  lon_vals = test_fct_aj(centroid_longitude_heart2, .x$lon),
+                  lat_vals = test_fct_aj(centroid_latitude_heart2, .x$lat))) -> mc_test_aj_heart2
+
+# find mid-colours btween deeppink and papaya
+# https://meyerweb.com/eric/tools/color-blend/#FF1493:E27231:2:hex
+
+scatter_plot_animate_heart2 <- mc_test_aj_heart2 %>% 
+  ggplot(aes(lon_vals, lat_vals)) + 
+  geom_point(color = '#F53372', size =2) +
+  #with_outer_glow(geom_point(color = '#E27231', size =2), colour='gold', sigma = 5, expand = 5) +
+  transition_time(t) + 
+  shadow_wake(0.5) +
+  annotate("text", y = 70, x = 170, label = "@allanjames1506", lineheight = 0.75, family = 'Zen',  size = 5, color = "gray40", vjust = 0.5)
+
+animate(scatter_plot_animate_heart2, fps = 30)
+
+# ANIMATE: McLaren heart central phase----
+
+grid_mclaren_heart3 <- read.csv('./00_raw_data/mclaren_logo_heart3_dots_horizontal_flip.csv') %>% 
+  select(2:3) %>% 
+  rename(lon = x, lat = y)
+
+p_mclaren_heart3 <- grid_mclaren_heart3 %>% 
+  ggplot(aes(lon, lat)) + 
+  geom_point(color = 'white') 
+
+p_mclaren_heart3
+
+p_mclaren_heart3_polygon <- grid_mclaren_heart3 %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
+  summarise(geometry = st_combine(geometry)) %>%
+  st_cast("POLYGON")
+
+st_centroid(p_mclaren_heart3_polygon)
+
+centroid_longitude_heart3 <- 140.4518
+centroid_latitude_heart3 <- 118.0075
+
+# grid_mclaren_heart1_slice <- grid_mclaren_heart1 %>% 
+#   slice(1:63)
+
+grid_mclaren_heart3 |>
+  group_split(lon, lat) %>% 
+  map_dfr(~tibble(t= 1:10,
+                  lon = .x$lon, 
+                  lat = .x$lat,
+                  lon_vals = test_fct_aj(centroid_longitude_heart3, .x$lon),
+                  lat_vals = test_fct_aj(centroid_latitude_heart3, .x$lat))) -> mc_test_aj_heart3
+
+# find mid-colours btween deeppink and papaya
+# https://meyerweb.com/eric/tools/color-blend/#FF1493:E27231:2:hex
+
+scatter_plot_animate_heart3 <- mc_test_aj_heart3 %>% 
+  ggplot(aes(lon_vals, lat_vals)) + 
+  geom_point(color = '#FF1493', size =2) +
+  #with_outer_glow(geom_point(color = '#E27231', size =2), colour='gold', sigma = 5, expand = 5) +
+  transition_time(t) + 
+  shadow_wake(0.5) +
+  annotate("text", y = 70, x = 170, label = "@allanjames1506", lineheight = 0.75, family = 'Zen',  size = 5, color = "gray40", vjust = 0.5)
+
+animate(scatter_plot_animate_heart3, fps = 30)
+
+# ANIMATE: 4 phased heart to McLaren speedmark----
+
+#grid_mclaren_heart3 == core heart --> core (1:10) --> split 1:5
+#grid_mclaren_heart2 == core heart to mclaren phase 1 --> phase1 (1:10)  --> split 6:10
+#grid_mclaren_heart1 == core heart to mclaren phase 2 --> phase2 (11:20) --> split 11:15
+#grid_mclaren_slice == mclaren speedmark --> speedmark (11:20) --> split 16:20
+
+
+grid_mclaren_heart3 |>
+  group_split(lon, lat) %>% 
+  map_dfr(~tibble(t= 1:10,
+                  lon = .x$lon, 
+                  lat = .x$lat,
+                  lon_vals = test_fct_aj(centroid_longitude_heart3, .x$lon),
+                  lat_vals = test_fct_aj(centroid_latitude_heart3, .x$lat))) -> core
+
+core <- core %>% 
+  mutate(colour = '#FF1493') %>% 
+  filter(t == 1 | t == 2 | t == 3 | t == 4 | t == 5)
+
+grid_mclaren_heart2 |>
+  group_split(lon, lat) %>% 
+  map_dfr(~tibble(t= 1:10,
+                  lon = .x$lon, 
+                  lat = .x$lat,
+                  lon_vals = test_fct_aj(centroid_longitude_heart3, .x$lon),
+                  lat_vals = test_fct_aj(centroid_latitude_heart3, .x$lat))) -> phase1
+
+phase1 <- phase1 %>% 
+  mutate(colour = '#F53372') %>% 
+  filter(t == 6 | t == 7 | t == 8 | t == 9 | t == 10)
+
+grid_mclaren_heart1 |>
+  group_split(lon, lat) %>% 
+  map_dfr(~tibble(t= 3:12,
+                  lon = .x$lon, 
+                  lat = .x$lat,
+                  lon_vals = test_fct_aj(centroid_longitude_heart3, .x$lon),
+                  lat_vals = test_fct_aj(centroid_latitude_heart3, .x$lat))) -> phase2
+
+phase2 <- phase2 %>%
+  mutate(colour = '#EC5352') %>% 
+  filter(t == 11 | t == 12)
+  
+grid_mclaren_slice |>
+  group_split(lon, lat) %>% 
+  map_dfr(~tibble(t= 11:20,
+                  lon = .x$lon, 
+                  lat = .x$lat,
+                  lon_vals = test_fct_aj(centroid_longitude_heart3, .x$lon),
+                  lat_vals = test_fct_aj(centroid_latitude_heart3, .x$lat))) -> speedmark
+
+speedmark <- speedmark %>%
+  mutate(colour = '#E27231') 
+#%>% 
+  filter(t == 16 | t == 17 | t == 18 | t == 19 | t == 20)
+  
+
+heart_to_speedmark <- bind_rows(core, phase1, speedmark)
+
+scatter_plot_animate_heart_to_speedmark <- heart_to_speedmark %>% 
+  ggplot(aes(lon_vals, lat_vals)) + 
+  geom_point(colour = heart_to_speedmark$colour, size =2) +
+  #with_outer_glow(geom_point(color = '#E27231', size =2), colour='gold', sigma = 5, expand = 5) +
+  transition_time(t) + 
+  shadow_wake(0.5) +
+  annotate("text", y = 70, x = 170, label = "@allanjames1506", lineheight = 0.75, family = 'Zen',  size = 5, color = "gray40", vjust = 0.5)
+
+animate(scatter_plot_animate_heart_to_speedmark, fps = 30, duration = 10)
+
+anim_save("./04_animate_gifs/first_saved_animation_heart_to_speedmark_logo_animate.gif", height = 372, width = 538, units = "px")
 
